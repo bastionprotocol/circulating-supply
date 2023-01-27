1581b28ef620094d3b606e7c1568c13230b743ef
pysrbastion <pysr@bastionprotocol.com>
2023-01-27 15:03:46 +0700

    feat: add script for reading current result

--- src/service.ts
+++ src/service.ts
@@ -1,15 +1,26 @@
+import "dotenv/config";
 import { Octokit } from "octokit";
 
-const octokit = new Octokit();
+if (!("GH_TOKEN" in process.env)) {
+  throw new Error("GH Token Not Found");
+}
+
+const octokit = new Octokit({
+  auth: process.env["GH_TOKEN"],
+});
 
 async function service() {
-  await octokit.request("GET /repos/{owner}/{repo}/contents/{path}", {
-    owner: "",
-    repo: "",
-    path: "circulating-supply",
-    message: "",
-    content: "",
-  });
+  const data = await octokit.request(
+    "GET /repos/{owner}/{repo}/contents/{path}",
+    {
+      owner: "bastionprotocol",
+      repo: "circulating-supply",
+      path: "package.json",
+      message: "",
+      content: "",
+    }
+  );
+  console.log("data", data);
 }
 
 service();
